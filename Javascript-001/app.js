235b00691b5dcc335cd876bd8348fc5b9c57102d
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-28 21:01:15 -0500

    Variables 1-18

--- Javascript-001/app.js
+++ Javascript-001/app.js
@@ -1,5 +1,11 @@
- document.querySelectorAll('btn').forEach(item =>) {
-      item.addEventListener('click',() =>){
-        alert('hello people')
-      })
-    })
+// Variable - Most Basics Building Block
+// Variables - Store, Access, Modify ===Value
+// Declare, Assignment Operator, Assign Value
+// Assign value later, modify existing
+
+let name = "john shrimp taco VI";
+let address, zip, state;
+address = "101 main street";
+zip = "60612";
+state = "CA";
+console.log(address, zip, state);
