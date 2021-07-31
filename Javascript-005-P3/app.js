3911887a6ff7e30c488bd66dcdd6fa32cadf1b87
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-07-31 12:52:28 -0400

    Update app.js

---
+++ Javascript-005-P3/app.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //card options
+  const cardArray = [
+    {
+      name: "blank",
+      img: "images/blank.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "whie",
+      img: "images/white.png",
+    },
+  ];
+});
