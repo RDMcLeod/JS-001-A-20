9f90b9c5c20c52bdf3a09bb941ff48489a871b35
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-07-31 13:15:30 -0400

    Update app.js and index

--- Javascript-005-P3/app.js
+++ Javascript-005-P3/app.js
@@ -2,10 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   //card options
   const cardArray = [
     {
-      name: "blank",
-      img: "images/blank.png",
-    },
-    {
       name: "cheeseburger",
       img: "images/cheeseburger.png",
     },
@@ -30,8 +26,29 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/pizza.png",
     },
     {
-      name: "whie",
+      name: "white",
       img: "images/white.png",
     },
   ];
+  const grid = document.querySelector(".grid");
+  var cardsChosen = [];
+  // create board
+
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement("img");
+      card.setAttribute("src", "images/blank.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", "flipCard");
+      grid.appendChild(card);
+    }
+  }
+  // check for matches
+  // flip your card
+  function flipCard() {
+    var cardId = this.getAttribute("data-id");
+    cardsChosen.push(cardArray[cardId]);
+  }
+
+  createBoard();
 });
