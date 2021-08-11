8598f7747cfcd6a100b67d775ad9796cf5471f1d
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-08-11 08:41:56 -0400

    Update files

--- Javascript-007-P4/app.js
+++ Javascript-007-P4/app.js
@@ -1,32 +1,35 @@
-const square = document.querySelectorAll(".square");
+const squares = document.querySelectorAll(".square");
 const mole = document.querySelectorAll(".mole");
 const timeLeft = document.querySelector("#time-Left");
-let score = document.querySelector("#score");
+const score = document.querySelector("#score");
 
 let result = 0;
-let currentTime = timeLeft.textContent;
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
 
 function randomSquare() {
-  square.forEach((className) => {
+  squares.forEach((className) => {
     className.classList.remove("mole");
   });
+
   let randomPosition = square[Math.floor.floor(Math.random() * 9)];
   randomPosition.classList.add("mole");
 
   // assign the id of the randomPostion to hitPoistion for us to use later
   hitPosition = randomPosition.id;
 }
-square.forEach((id) => {
-  id.addEventListener("mouseup", () => {
-    if (id.id === hitPosition) {
-      result = result + 1;
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
       score.textContent = result;
+      hitPosition = null;
     }
   });
 });
 function moveMole() {
-  let timerId = null;
-  timerId = setInterval(randomSquare, 1000);
+  timerId = setInterval(randomSquare, 500);
 }
 
 moveMole();
@@ -35,9 +38,10 @@ function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime;
 
-  if (currentTime === 0) {
+  if (currentTime == 0) {
     clearInterval(timerId);
-    alert("GAME OVER! Your final score is");
+    clearInterval(timerId);
+    alert("GAME OVER! Your final score is" + result);
   }
 }
-let timerId = setInterval(countDown, 1000);
+let countDownTimerId = setInterval(countDown, 1000);
