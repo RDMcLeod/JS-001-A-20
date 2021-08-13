847cbaf802b85eb2560dbf690091be79e5154233
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-08-13 17:57:26 -0400

    Update app.js

---
+++ Javascript-009-P5/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMCom=ntentLoaded", ()=>{
+    const squares = document.querySelectorAll(".grid div")
+    const result = document.querySelector("result")
+    const displayCurrentPlayer = document.querySelector("current-player")
+    let displayCurrentPlayer = 1
+
+    for (var i = 0, len = square.length; i < len; i++)
+    (function (index)){
+        // add and onclick to each sqauer in grid
+        squares[i].onclick = function (){
+            
+
+        }
+    }
+
+    }
+})
