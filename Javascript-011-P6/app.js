14b4f81c86bd6f12359ae2c3906c41cc335eedd9
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-08-20 12:02:15 -0400

    Upfate app.js

--- Javascript-011-P6/app.js
+++ Javascript-011-P6/app.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const squares = document.querySelectorAll("grid div");
   const scoreDisplay = document.querySelector("span");
-  const startBin = document.querySelector(".start");
+  const startBtn = document.querySelector(".start");
+
+  const width  10
+  let currentIndex = 0 // so first st grid on grid
+  let appleIndex = 0 // so first dive in grid
+  let currentSnake = [2,1,0] // all dive with a 2 will be the head and divs with 0 will be the tail - body parts will be 1's
+  let direction = 1
+  let score = 0
+  let speed = 0.9
+  let intervalTime = 0
+  let interval = 0
+
+  // assign functions to keycodes
+
+
+
 });
