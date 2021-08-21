b1cd0d86655f53e8a0019659cc73abc1a1eb1767
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-08-21 12:50:12 -0400

    Update app.js

--- Javascript-011-P6/app.js
+++ Javascript-011-P6/app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const scoreDisplay = document.querySelector("span");
   const startBtn = document.querySelector(".start");
 
-  const width  10
+  const width = 10
   let currentIndex = 0 // so first st grid on grid
   let appleIndex = 0 // so first dive in grid
   let currentSnake = [2,1,0] // all dive with a 2 will be the head and divs with 0 will be the tail - body parts will be 1's
@@ -13,18 +13,37 @@ document.addEventListener("DOMContentLoaded", () => {
   let intervalTime = 0
   let interval = 0
 
+
+  // to start game and restart game
+  function startGame (){
+    currentSnake.forEach(index => squares[index].classList.remove ("snake"))
+    squares[appleIndex].classList.remove("apple")
+    clearInterval(interval)
+    score = 0
+    // randomApple()
+    direction = 1
+    scoreDisplay.innerText = score
+    intervalTime = 0
+    currentSnake = [2,1,0]
+    currentIndex = 0
+  }
+
+
+
+
   // assign functions to keycodes
  function control(e) {
    squares [currentIndex].classList.remove("snake") //removing the class of snake
 
    if (e.keyCode === 39){
      direction = 1 // if we pres the right arrow key on keyboard, the snake wil go right
-
    }else if (e.keyCode = 38){
-     direction = -width // if we press the up arrow the snake will go back 10 divs appaearing to go up
-   }else if (e.ketCode === 37)
+   direction = -width // if we press the up arrow the snake will go back 10 divs appaearing to go up
+   }else if (e.keyCode === 37)
    direction -1 // if we press left the snake will go left 1 div
+   }else if (e.keyCode === 40){
+   direction = +width // 
  }
-
-
+}
+document.addEventListener("keyup", control)
 });
