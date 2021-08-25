033e2103a7315d1c09cfd26f1ce885a478840584
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-08-25 15:33:16 -0400

    Update app.js

--- Javascript-013-P7/app.js
+++ Javascript-013-P7/app.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const squares = document.querySelectorAll("grid div");
-  const resultDisplay = document.querySelector("result");
+  const resultDisplay = document.querySelector("results");
   let width = 15;
   let currentShooterIndex = 202;
   let currentInvaderIndex = 0;
+  let alienInvadersTakendown = []
+  let result = 0
+  let direction = 1
+  Let invaderId
 });
