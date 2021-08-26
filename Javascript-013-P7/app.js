c913236706ec5b8153765fbbbcfc412fac3eeb64
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-08-26 19:44:19 -0400

    Update app.js

--- Javascript-013-P7/app.js
+++ Javascript-013-P7/app.js
@@ -4,17 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
   let width = 15;
   let currentShooterIndex = 202;
   let currentInvaderIndex = 0;
-  let alienInvadersTakendown = []
-  let result = 0
-  let direction = 1
-  Let invaderId
+  let alienInvadersTakendown = [];
+  let result = 0;
+  let direction = 1;
 
   //define the alien invaders
   const alienInvaders = [
-    1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39
-  ]
- // draw the alien invaders
- alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].clasList.add("invader"))
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+  ];
+  // draw the alien invaders
+  alienInvaders.forEach((invader) =>
+    squares[currentInvaderIndex + invader].clasList.add("invader")
+  );
 
+  // draw the shooter
+  squares[currentShooterIndex].classList.add("shooter");
 
+  // move the shooter across the grid line
+  function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+    case 'ArrowLeft':
+      if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+      break
+    case 'ArrowRight' :
+      if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+      break
+  }
 });
