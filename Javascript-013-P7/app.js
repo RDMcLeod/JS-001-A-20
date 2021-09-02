ab7c8161d3e02a6b6c4d715311b67d8a1b308182
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-09-02 09:14:56 -0400

    Update app.js

--- Javascript-013-P7/app.js
+++ Javascript-013-P7/app.js
@@ -1,5 +1,5 @@
-const squares = document.querySelector(".grid");
-const resultDisplay = document.querySelector(".results");
+const grid = document.querySelector(".grid");
+const resultsDisplay = document.querySelector(".results");
 let currentShooterIndex = 202;
 let width = 15;
 let direction = 1;
@@ -17,8 +17,8 @@ const squares = Array.from(document.querySelectorAll(".grid div"));
 
 //define the alien invaders
 const alienInvaders = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31,
+  32, 33, 34, 35, 36, 37, 38, 39,
 ];
 
 function draw() {
@@ -39,9 +39,10 @@ function remove() {
 squares[currentShooterIndex].classList.add("shooter");
 
 // move the shooter across the grid line
+
 function moveShooter(e) {
   squares[currentShooterIndex].classList.remove("shooter");
-  switch (e.keyCode) {
+  switch (e.key) {
     case "ArrowLeft":
       if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
       break;
@@ -54,6 +55,7 @@ function moveShooter(e) {
 document.addEventListener("keydown", moveShooter);
 
 // move the alien invaders
+
 function moveInvaders() {
   const leftEdge = alienInvaders[0] % width === 0;
   const rightEdge =
