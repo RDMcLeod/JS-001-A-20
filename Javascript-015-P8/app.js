4ee9e058b419fbb02775bb74c90b42ae4c3d00c7
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-09-07 17:07:19 -0400

    Update app.js move logs right

--- Javascript-015-P8/app.js
+++ Javascript-015-P8/app.js
@@ -108,3 +108,28 @@ function autoMoveLogs() {
       break
     }
   }
+   //move the log right on the time loop
+  function moveLogRight(logRight) {
+    switch (true) {
+      case logRight.classList.contains('l1'):
+      logRight.classList.remove('l1')
+      logRight.classList.add('l5')
+      break
+      case logRight.classList.contains('l2'):
+      logRight.classList.remove('l2')
+      logRight.classList.add('l1')
+      break
+      case logRight.classList.contains('l3'):
+      logRight.classList.remove('l3')
+      logRight.classList.add('l2')
+      break
+      case logRight.classList.contains('l4'):
+      logRight.classList.remove('l4')
+      logRight.classList.add('l3')
+      break
+      case logRight.classList.contains('l5'):
+      logRight.classList.remove('l5')
+      logRight.classList.add('l4')
+      break
+    }
+  }
