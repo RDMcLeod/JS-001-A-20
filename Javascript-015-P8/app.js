0672fdd7a79069afcc44d206eeb490c875819b6f
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-09-07 16:58:15 -0400

    update app.js move logs left

--- Javascript-015-P8/app.js
+++ Javascript-015-P8/app.js
@@ -83,3 +83,28 @@ function autoMoveLogs() {
     logsLeft.forEach(logLeft => moveLogLeft(logLeft))
     logsRight.forEach(logRight => moveLogRight(logRight))
   }
+  // move the log left on the time loop
+   function moveLogLeft(logLeft) {
+    switch (true) {
+      case logLeft.classList.contains('l1'):
+      logLeft.classList.remove('l1')
+      logLeft.classList.add('l2')
+      break
+      case logLeft.classList.contains('l2'):
+      logLeft.classList.remove('l2')
+      logLeft.classList.add('l3')
+      break
+      case logLeft.classList.contains('l3'):
+      logLeft.classList.remove('l3')
+      logLeft.classList.add('l4')
+      break
+      case logLeft.classList.contains('l4'):
+      logLeft.classList.remove('l4')
+      logLeft.classList.add('l5')
+      break
+      case logLeft.classList.contains('l5'):
+      logLeft.classList.remove('l5')
+      logLeft.classList.add('l1')
+      break
+    }
+  }
