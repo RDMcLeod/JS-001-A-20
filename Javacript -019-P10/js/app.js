4a5476befbd5558b900343b03347f2794689beac
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-09-16 21:42:46 -0400

    Update app.js & style.css

--- Javacript -019-P10/js/app.js
+++ Javacript -019-P10/js/app.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+
+  const grid = document.querySelector("grid")
   const width = 10;
   const height = 20;
+
+  // tetrominoes
+  const l Tetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2, width*2+2],
+    
+
+  ]
 });
