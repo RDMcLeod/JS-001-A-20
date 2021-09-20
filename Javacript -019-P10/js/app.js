fda8191a4081b167d54b1bf0ceab6a1511efb797
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-09-20 19:01:07 -0400

    Update app.js - arrays

--- Javacript -019-P10/js/app.js
+++ Javacript -019-P10/js/app.js
@@ -17,22 +17,32 @@ document.addEventListener("DOMContentLoaded", () => {
     [width + 1, width + 2, width * 2, width * 2 + 1],
   ];
   const tTetromino = [
-    [1, width , width + 1,width+ 2],
+    [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width 2+1],
-    [1, width, width +1, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
   ];
   const oTetromino = [
-    [0, 1, width, width+1],
-    [0, 1, width, width+1],
-    [0, 1, width, width+1],
-    [0, 1, width, width+1]
-    
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
   ];
   const iTetromino = [
-    [1, width + 1, width * 2 + 1, width*3+1],
-    [width, width + 1, width + 2, width +3],
-    [1, width + 1, width * 2 + 1, width*3+1],
-    [width, width + 1, width + 2, width +3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
   ];
+  const theTetrominos = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+  // randomly select Tetromino
+  let random = Math.floor(math.random() * theTetrominos.length);
+  let currentRotation = 0;
+  let current = theTetrominos[random];
 });
