e16cb077ae32e44b4d1986b73296913e02192b11
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-09-20 18:48:49 -0400

    Update app.js

--- Javacript -019-P10/js/app.js
+++ Javacript -019-P10/js/app.js
@@ -12,26 +12,27 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
   const zTetromino = [
     [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2, width * 2 + 2],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
   ];
   const tTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2, width * 2 + 2],
+    [1, width , width + 1,width+ 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width 2+1],
+    [1, width, width +1, width * 2 + 1],
   ];
   const oTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2, width * 2 + 2],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+    
   ];
   const iTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width*3+1],
+    [width, width + 1, width + 2, width +3],
+    [1, width + 1, width * 2 + 1, width*3+1],
+    [width, width + 1, width + 2, width +3],
   ];
 });
