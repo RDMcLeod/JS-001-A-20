f972e7745df43cacfbe26f9d42570609d8e6344d
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-10-08 23:04:59 -0400

    Update script.js

--- Javascript-017-P9/script.js
+++ Javascript-017-P9/script.js
@@ -13,10 +13,10 @@ function blurring() {
   }
 
   loadText.innerText = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load. 0, 100, 30, 0)})`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 // reference - SO  from BT
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
