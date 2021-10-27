3ee17bc2421fa825c3495a48ce3d02ec24924192
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-10-27 19:35:20 -0400

    Update script.js

--- Javascript-025-P13/script.js
+++ Javascript-025-P13/script.js
@@ -4,4 +4,15 @@ const listItems = [];
 
 async function getData() {
   const res = await fetch("https://randomuser.me/api?results=50");
+
+  const data = await res.json;
+
+  // clear results
+  results.innerHTML = "";
+
+  results.forEach((user) => {
+    const li = document.createElement("li");
+
+    listItems.push(li);
+  });
 }
