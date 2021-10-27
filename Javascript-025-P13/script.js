5535eb6734913fa86bad9f81ad9dcd108f51a51c
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-10-27 19:47:57 -0400

    Update script

--- Javascript-025-P13/script.js
+++ Javascript-025-P13/script.js
@@ -14,5 +14,13 @@ async function getData() {
     const li = document.createElement("li");
 
     listItems.push(li);
+
+    li.innerHTML = `
+    <img src="${user.picture.large}"alt ="${user.name.first}"
+    <div class = "user-info">
+    <h4>${user.name.first} ${user.name.last}</h4>
+
+    </div>
+    `;
   });
 }
