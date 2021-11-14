2eec589493af2504adee70511fd7ed235d2ca7b6
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-11-13 21:10:29 -0500

    Update script

--- Javascript-031-P16/script.js
+++ Javascript-031-P16/script.js
@@ -4,8 +4,13 @@ function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
+  const seconds = (newYearsDate - currentDate) * 1000;
+
+  const days = seconds / 3600 / 24;
+
   console.log(newYearsDate - currentDate);
 }
 
+//Initial Call
 setInterval(countdown, 1000);
 countdown();
