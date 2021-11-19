405d17a11943f365d3d024e33cb7cb39f76d57cb
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-11-18 19:26:27 -0500

    Update script.js

--- Javascript-031-P16/script.js
+++ Javascript-031-P16/script.js
@@ -1,3 +1,8 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
 const newYears = "1 Jan 2021";
 
 function countdown() {
@@ -8,12 +13,15 @@ function countdown() {
 
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const seconds = totalSeconds % 60;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days, hours, minutes);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
 }
 
 //Initial Call
-setInterval(countdown, 1000);
 countdown();
+setInterval(countdown, 1000);
