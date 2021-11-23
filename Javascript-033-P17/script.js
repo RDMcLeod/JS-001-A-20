2cf0cae117b9039fa6c94dabb65e857cc19c12fe
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-11-22 21:39:25 -0500

    Update script.js

--- Javascript-033-P17/script.js
+++ Javascript-033-P17/script.js
@@ -1,10 +1,18 @@
 const quizData = [
   {
     question: "how old is d-man",
-    a_1: "25",
-    a_2: "36",
-    a_3: '45',
-    a_4: '63',
-    correct: 
+    a: "25",
+    b: "36",
+    c: "45",
+    d: "63",
+    correct: "c",
+  },
+  {
+    question: " what is the most used programming language in 2021",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "Javascript",
+    correct: "d",
   },
 ];
