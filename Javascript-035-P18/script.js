266f5329c9dad564e49408f595b2687c42c83cb9
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-12-16 21:14:58 -0500

    update script.js

--- Javascript-035-P18/script.js
+++ Javascript-035-P18/script.js
@@ -1,8 +1,14 @@
-function getRandomMeal() {
-  fetch("http://www.themealdb.com /api/json/v1/1/random.php");
+async function getRandomMeal() {
+  const randomMeal = fetch(
+    "http://www.themealdb.com /api/json/v1/1/random.php"
+  );
 }
-function getMealById(id) {
-  fetch("http://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+async function getMealById(id) {
+  const meal = fetch(
+    "http://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
 }
 
-function getMealsBySearch(term) {}
+async function getMealsBySearch(term) {
+  const meals = fetch("www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+}
