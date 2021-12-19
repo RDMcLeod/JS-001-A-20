6c3f3e356f82f8ba53767c8bee2d1943a58016d1
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-12-18 21:33:40 -0500

    Update scritp.js

--- Javascript-035-P18/script.js
+++ Javascript-035-P18/script.js
@@ -1,14 +1,19 @@
+getRandomMeal();
+
 async function getRandomMeal() {
-  const randomMeal = fetch(
+  const randomMeal = await fetch(
     "http://www.themealdb.com /api/json/v1/1/random.php"
   );
+  const randomMeal = await Response.json();
 }
 async function getMealById(id) {
-  const meal = fetch(
+  const meal = await fetch(
     "http://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
   );
 }
 
 async function getMealsBySearch(term) {
-  const meals = fetch("www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+  const meals = await fetch(
+    "www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
 }
