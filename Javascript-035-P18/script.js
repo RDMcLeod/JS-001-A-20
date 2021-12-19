a255025c0e9a5b6650e5c7859b5edbb92a355b18
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-12-18 22:27:08 -0500

    Update script

--- Javascript-035-P18/script.js
+++ Javascript-035-P18/script.js
@@ -4,7 +4,10 @@ async function getRandomMeal() {
   const randomMeal = await fetch(
     "http://www.themealdb.com /api/json/v1/1/random.php"
   );
-  const randomMeal = await Response.json();
+  const respData = await Resp.json();
+  const randomMeal = respData.meals[0];
+
+  addMeal(randomMeal, true);
 }
 async function getMealById(id) {
   const meal = await fetch(
@@ -16,4 +19,8 @@ async function getMealsBySearch(term) {
   const meals = await fetch(
     "www.themealdb.com/api/json/v1/1/search.php?s=" + term
   );
+}
+addMeal(mealData,random = false){
+  const meal = document.createElement('div');
+  meal.classList.add('meal');
 }
