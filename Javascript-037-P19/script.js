b52cf54bb3f4d899ee474aef0c7e32a08a39cd52
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-12-29 23:57:00 -0500

    update script.js

--- Javascript-037-P19/script.js
+++ Javascript-037-P19/script.js
@@ -1,4 +1,33 @@
-const notesEl = document.querySelector(".notes");
+onst addBtn = document.getElementById("add");
+
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+    notes.forEach((note) => {
+        addNewNote(note);
+    });
+}
+
+addBtn.addEventListener("click", () => {
+    addNewNote();
+});
+
+function addNewNote(text = "") {
+    const note = document.createElement("div");
+    note.classList.add("note");
+
+    note.innerHTML = `
+        <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>
+        </div>
+    `;
+
+
 const editBtn = document.querySelector(".edit");
 const deleteBtn = document.querySelector(".delete");
 
