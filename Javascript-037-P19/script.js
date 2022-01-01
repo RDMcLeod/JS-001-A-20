2bc54bb750c84e87bfac6d9c3e74d9fe61ed0420
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-12-31 23:58:35 -0500

    Update script.js

--- Javascript-037-P19/script.js
+++ Javascript-037-P19/script.js
@@ -15,14 +15,19 @@ function addNewNote(text = "") {
   const note = document.createElement("div");
   note.classList.add("note");
 
-  document.body.appendChild('note');
+  note.innerHTML = `
+   <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>
+        </div>
+    `;
 
-}
-
-  
-  const notesEl = document.querySelector('.notes')
-  const editBtn = document.querySelector(".edit");
-  const deleteBtn = document.querySelector(".delete");
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
 
   const main = note.querySelector(".main");
   const textArea = note.querySelector("textarea");
@@ -32,17 +37,15 @@ function addNewNote(text = "") {
     textArea.classList.toggle("hidden");
   });
 
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
   textArea.addEventListener("input", (e) => {
     const { value } = e.target;
+
+    main.innerHTML = marked(value);
   });
+
+  document.body.appendChild("note");
 }
-//note.innerHTML = `
-        //<div class="notes">
-            //<div class="tools">
-                //<button class="edit"><i class="fas fa-edit"></i></button>
-                //<button class="delete"><i class="fas fa-trash-alt"></i></button>
-            //</div>
-            //div class="main ${text ? "" : "hidden"}"></div>
-            //<textarea class="${text ? "hidden" : ""}"></textarea>
-        //</div>
-    //`;
