79e03467bdeb60a2fcc8377244b4720e66522770
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-12-31 23:49:09 -0500

    Update script.j & css

--- Javascript-037-P19/script.js
+++ Javascript-037-P19/script.js
@@ -1,5 +1,4 @@
 const addBtn = document.getElementById("add");
-
 const notes = JSON.parse(localStorage.getItem("notes"));
 
 if (notes) {
@@ -16,17 +15,12 @@ function addNewNote(text = "") {
   const note = document.createElement("div");
   note.classList.add("note");
 
-  note.innerHTML = `
-        <div class="notes">
-            <div class="tools">
-                <button class="edit"><i class="fas fa-edit"></i></button>
-                <button class="delete"><i class="fas fa-trash-alt"></i></button>
-            </div>
-            <div class="main ${text ? "" : "hidden"}"></div>
-            <textarea class="${text ? "hidden" : ""}"></textarea>
-        </div>
-    `;
+  document.body.appendChild('note');
 
+}
+
+  
+  const notesEl = document.querySelector('.notes')
   const editBtn = document.querySelector(".edit");
   const deleteBtn = document.querySelector(".delete");
 
@@ -42,3 +36,13 @@ function addNewNote(text = "") {
     const { value } = e.target;
   });
 }
+//note.innerHTML = `
+        //<div class="notes">
+            //<div class="tools">
+                //<button class="edit"><i class="fas fa-edit"></i></button>
+                //<button class="delete"><i class="fas fa-trash-alt"></i></button>
+            //</div>
+            //div class="main ${text ? "" : "hidden"}"></div>
+            //<textarea class="${text ? "hidden" : ""}"></textarea>
+        //</div>
+    //`;
