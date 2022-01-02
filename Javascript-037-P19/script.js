9cc39d889aa8c00bec1e774e358b99cbb8895c6d
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-01 22:43:43 -0500

    Update script.js

--- Javascript-037-P19/script.js
+++ Javascript-037-P19/script.js
@@ -1,12 +1,6 @@
 const addBtn = document.getElementById("add");
 const notes = JSON.parse(localStorage.getItem("notes"));
 
-if (notes) {
-  notes.forEach((note) => {
-    addNewNote(note);
-  });
-}
-
 addBtn.addEventListener("click", () => {
   addNewNote();
 });
