ef36e2712929464ac015fdf9d6c325d03cce7180
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-04 19:53:12 -0500

    update files

---
+++ Javascript-039-P20/script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todos = document.getElementById('todos')
+
+
+
+form.addEventListener('submit',)(e) => {
+    e.preventDefault();
+
+    const todo = input.value;
+
+    if (todo) {
+        const todoEl = document.createElement
+        ('li');
+
+               
+    }
+
+
+}
