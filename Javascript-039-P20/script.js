f578cc8d3404374c07f2d1f2093a62a734894e7b
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-05 21:19:27 -0500

    Update text decoration & small click notetext

--- Javascript-039-P20/script.js
+++ Javascript-039-P20/script.js
@@ -5,6 +5,10 @@ const todos = document.getElementById("todos");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  AddTodo();
+});
+
+function AddTodo() {
   const todoText = input.value;
 
   if (todoText) {
@@ -24,4 +28,4 @@ form.addEventListener("submit", (e) => {
 
     input.value = "";
   }
-});
+}
