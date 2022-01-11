37914c4809cdd9dbe460ad4289f012821251e2e3
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-10 23:35:25 -0500

    Update script & css

--- Javascript-039-P20/script.js
+++ Javascript-039-P20/script.js
@@ -9,13 +9,6 @@ async function getMovies() {
 
   console.log(respData);
 
-  respData.results.forEach((movie) => {
-    const omg = document.createElement("img");
-    IMGPATH.src = IMGPATH + movie.poster_path;
-
-    document.body.appendChild(img);
-  });
-
   return respData;
 }
 getmovies();
