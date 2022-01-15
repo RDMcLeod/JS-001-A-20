9075643c5ddcb2edd9293330777d97c0deb967d3
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-15 15:21:08 -0500

    Constr cir func

---
+++ Javascript-040-P21/script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousedown", () => {});
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+}
