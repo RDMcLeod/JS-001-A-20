91ad9c14d2e09a89a76c64d5c8fa5ba21a061095
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-18 20:53:39 -0500

    revise script-animations

--- Javascript-040-P21/script.js
+++ Javascript-040-P21/script.js
@@ -2,13 +2,20 @@ const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
 
-let size = 20;
+let size = 30;
 
 canvas.addEventListener("mousedown", () => {});
 
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
-  ctx.stroke();
+  ctx.fill();
 }
 drawCircle(50, 50);
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircle(50, 50);
+  requestAnimationFrame(draw);
+}
+draw();
