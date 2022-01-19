3c854e8992a1f7e3004ffc1010d617ba82909909
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-18 21:19:07 -0500

    animationerror

--- Javascript-040-P21/script.js
+++ Javascript-040-P21/script.js
@@ -2,20 +2,25 @@ const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
 
-let size = 30;
+let size = 20;
 
-canvas.addEventListener("mousedown", () => {});
+canvas.addEventListener("mousemove", (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+
+  drawCircle(x, y);
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
   ctx.fill();
 }
-drawCircle(50, 50);
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle(50, 50);
-  requestAnimationFrame(draw);
-}
-draw();
+//function draw() {
+//  ctx.clearRect(0, 0, canvas.width, canvas.height);
+//  drawCircle(x++-, y);
+//  requestAnimationFrame(draw);
+//}
+
+//draw();
