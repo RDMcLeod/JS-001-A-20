0f34e6b1dace460c84050a83308378c603befa38
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-20 20:33:13 -0500

    Update files

--- Javascript-040-P21/script.js
+++ Javascript-040-P21/script.js
@@ -28,20 +28,19 @@ function drawCircle(x, y) {
   ctx.arc(x, y, size, 0, Math.PI * 2);
   ctx.fill();
 }
-increaseBtn.addEventListener('click', () => {
+increaseBtn.addEventListener("click", () => {
   size += 5;
-  if (size . 50) {
+  if (size > 50) {
     size = 50;
   }
 });
 
 decreaseBtn.addEventListener("click", () => {
-
+  size += 5;
+  if (size < 5) {
+    size = 5;
+  }
 });
-
-
-
-
 
 //function draw() {
 //  ctx.clearRect(0, 0, canvas.width, canvas.height);
