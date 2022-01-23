e61274fb48a902add723003f2bf013b1cd3b67c2
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-22 19:06:30 -0500

    update script

--- Javascript-040-P21/script.js
+++ Javascript-040-P21/script.js
@@ -1,12 +1,13 @@
 const canvas = document.getElementById("canvas");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
-const sizeEl = document.getElementById("decrease");
-
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
 const ctx = canvas.getContext("2d");
 
 let size = 20;
 let isPressed = false;
+let color = "black";
 
 canvas.addEventListener("mousedown", () => {
   isPressed = true;
