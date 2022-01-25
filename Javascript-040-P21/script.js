5eea24ac2fca059e3d76d970d6f14c08ba6d5584
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-01-24 23:23:54 -0500

    update script brush animationss

--- Javascript-040-P21/script.js
+++ Javascript-040-P21/script.js
@@ -8,12 +8,21 @@ const ctx = canvas.getContext("2d");
 let size = 20;
 let isPressed = false;
 let color = "black";
+let x = undefined;
+let y = undefined;
 
 canvas.addEventListener("mousedown", () => {
   isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
 });
+
 canvas.addEventListener("mouseup", () => {
   isPressed = false;
+
+  x = undefined;
+  y = undefined;
 });
 
 canvas.addEventListener("mousemove", (e) => {
@@ -31,6 +40,14 @@ function drawCircle(x, y) {
   ctx.fillStyle = color;
   ctx.fill();
 }
+function line(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
 increaseBtn.addEventListener("click", () => {
   size += 5;
   if (size > 50) {
