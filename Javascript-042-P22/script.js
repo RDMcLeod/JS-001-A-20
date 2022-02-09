2e8841e8379c43d4a1d84e601cc149d607ddb501
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-02-08 23:10:44 -0500

    edit script.js

--- Javascript-042-P22/script.js
+++ Javascript-042-P22/script.js
@@ -53,6 +53,10 @@ function generateX() {
   if (symbolEl.checked) {
     xs.push(getSymbol());
   }
+
+  if (xs.length === 0) return "";
+
+  return xs[Math.floor(Math.random() * xs.length)];
 }
 
 generateEl.addEventListener("click", generatePassword);
