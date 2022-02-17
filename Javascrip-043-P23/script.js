f1bb625ea4fe48ae78df95e743636d1154cf5be6
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-02-16 23:37:37 -0500

    edits script.js'

--- Javascrip-043-P23/script.js
+++ Javascrip-043-P23/script.js
@@ -1,11 +1,13 @@
-getweatherbylocation("London");
-const APIURL =
-  "https://cors-anywhere.herokuapp.com/http://www.metaweather.com/api/";
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
-async function getweatherbylocation(location) {
-  const resp = await fetch(APIURL + "/location/search/?query=" + location, {});
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), { origin: "cors" });
   const respData = await resp.json();
 
   console.log(respData);
+
+  addWeatherToPage(respData);
 }
-getweatherbylocation("London");
