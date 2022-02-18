fb156b9da18ac8bad956cbaa477900edb217c377
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-02-17 22:58:48 -0500

    script.js edits

--- Javascrip-043-P23/script.js
+++ Javascrip-043-P23/script.js
@@ -1,13 +1,12 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
-const url = (city) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+const url = (location) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
 
-async function getWeatherByLocation(city) {
-  const resp = await fetch(url(city), { origin: "cors" });
+async function getWeatherByLocation(location) {
+  const resp = await fetch(url(location), { origin: "cors" });
   const respData = await resp.json();
 
   console.log(respData);
-
-  addWeatherToPage(respData);
 }
+getWeatherByLocation("London");
