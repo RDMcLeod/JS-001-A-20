a89ef7b5deebcd9a4ee4c121e9c4571084142cb3
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-02-19 20:34:00 -0500

    edits all files

--- Javascrip-043-P23/script.js
+++ Javascrip-043-P23/script.js
@@ -1,5 +1,9 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 const url = (location) =>
   `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
 
@@ -12,8 +16,33 @@ async function getWeatherByLocation(location) {
   addWeatherToPage(respData);
 }
 
-function addWeatherToPage(data) {}
+function addWeatherToPage(data) {
+  const temp = KtoC(data.main.temp);
 
-function FtoC(K) {
-  return (K - 273.15).toFixed(2);
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+
+  weather.innerHTML = `
+        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+        <small>${data.weather[0].main}</small>
+    `;
+
+  // cleanup
+  main.innerHTML = "";
+
+  main.appendChild(weather);
 }
+
+function KtoC(K) {
+  return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+
+  if (city) {
+    getWeatherByLocation(city);
+  }
+});
