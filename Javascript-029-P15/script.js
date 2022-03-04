3b2787048e15dc928334cce55b4d70991adb545f
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-03-03 22:43:43 -0500

    Update css & script

--- Javascript-029-P15/script.js
+++ Javascript-029-P15/script.js
@@ -5,6 +5,10 @@ const todos = document.getElementById("todos");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  addTodo();
+});
+
+function addTodo() {
   const todoText = input.value;
 
   if (todoText) {
@@ -25,4 +29,4 @@ form.addEventListener("submit", (e) => {
 
     input.value = "";
   }
-});
+}
