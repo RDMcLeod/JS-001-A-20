fe63496bd05629e8ed1a11c77eb7a26a3e69f99f
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-03-08 20:40:54 -0500

    edits script/final

--- Javascript-029-P15/script.js
+++ Javascript-029-P15/script.js
@@ -2,7 +2,13 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
-const todos - JSON.parse(localStorage.getItem('todos'));
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todo.forEach((todo) => {
+    addTodo(todo);
+  });
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,11 +16,19 @@ form.addEventListener("submit", (e) => {
   addTodo();
 });
 
-function addTodo() {
-  const todoText = input.value;
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
 
   if (todoText) {
     const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
     todoEl.innerText = todoText;
 
     todoEl.addEventListener("click", () => {
