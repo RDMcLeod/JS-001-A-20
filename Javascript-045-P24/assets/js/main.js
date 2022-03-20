ee3d8c81804e52778a7e79e4fb83af6188f55ee7
RDMcLeod <mcleod.r.derrick@gmail.com>
2022-03-20 17:48:57 -0400

    re-org files

---
+++ Javascript-045-P24/assets/js/main.js
@@ -0,0 +1,33 @@
+// Grab elements
+
+//Nav styles on scroll
+
+// Open menu & search pop-up
+
+// Open/Close search form popup
+
+// -- Close the search form popup on ESC keypress
+
+// Switch theme/add to local storage
+
+// Check to see if there is a theme preference in local Storage, if so add the ligt theme to the body
+
+// Add light theme on click
+
+// If the body has the class of light theme then add it to local Storage, if not remove it
+
+// Swiper
+
+// How many slides to show
+
+// How much space between slides
+
+// Make the next and previous buttons work
+
+// Make the pagination indicators work
+
+//Responsive breakpoints for how many slides to show at that view
+
+// 700px and up shoes 2 slides
+
+// 1200px and up shoes 3 slides
